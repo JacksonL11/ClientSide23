216357a64c8ea0cd5021e7a88fc5d8149d0495ce
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-01-31 10:02:51 -0600

    Update javascript.js

--- CaseProject/Shop/javascript.js
+++ CaseProject/Shop/javascript.js
@@ -1,11 +1,11 @@
 function startTimer(duration, display) {
   var timer = duration;
   setInterval(function () {
-    display.textContent = --timer + " secs";
+    display.textContent = --timer + "hours";
     if (timer === 0) {
       clearInterval(this);
     }
-  }, 1000);
+  }, 24);
 }
 
 
