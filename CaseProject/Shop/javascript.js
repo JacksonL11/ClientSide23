3bd78f704c5c88591eff5a747f05616808db4d80
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-01-31 22:17:38 -0600

    Update javascript.js

--- CaseProject/Shop/javascript.js
+++ CaseProject/Shop/javascript.js
@@ -1,1 +1,15 @@
+document.addEventListener("DOMContentLoaded", function() {
+  var hoursLeft = 24;
 
+  function countDown() {
+    hoursLeft--;
+    document.getElementById("countdown").innerHTML = "Hours left in the day: " + hoursLeft;
+
+    if (hoursLeft === 0) {
+      clearInterval(timer);
+      document.getElementById("countdown").innerHTML = "Time's up!";
+    }
+  }
+
+  var timer = setInterval(countDown, 3600 * 1000);
+});
