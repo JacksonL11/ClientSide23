ae68a30ca4815f29cd9ef98e8f4becae883b4d9f
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-01-31 21:13:17 -0600

    Update and rename javascriptt.js to javascript.js

---
+++ CaseProject/Shop/javascript.js
@@ -0,0 +1,14 @@
+
+var hoursLeft = 24;
+
+function countDown() {
+  hoursLeft--;
+  document.getElementById("countdown").innerHTML = "Hours left in the day: " + hoursLeft;
+
+  if (hoursLeft === 0) {
+    clearInterval(timer);
+    document.getElementById("countdown").innerHTML = "Time's up!";
+  }
+}
+
+var timer = setInterval(countDown, 3600 * 1000);
