e33c27972faf97b492f30dbb845a6f764d1a9bb0
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-02-10 09:09:30 -0600

    Update javascript.js

--- CaseProject/Shop/javascript.js
+++ CaseProject/Shop/javascript.js
@@ -1,44 +1,18 @@
-/*
-	Add to cart fly effect with jQuery. - May 05, 2013
-	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
-	license: https://www.opensource.org/licenses/mit-license.php
-*/   
+const items = document.querySelectorAll(".item");
+const shoppingCart = [];
 
-$('.add-to-cart').on('click', function () {
-        var cart = $('.shopping-cart');
-        var imgtodrag = $(this).parent('.item').find("img").eq(0);
-        if (imgtodrag) {
-            var imgclone = imgtodrag.clone()
-                .offset({
-                top: imgtodrag.offset().top,
-                left: imgtodrag.offset().left
-            })
-                .css({
-                'opacity': '0.5',
-                    'position': 'absolute',
-                    'height': '150px',
-                    'width': '150px',
-                    'z-index': '100'
-            })
-                .appendTo($('body'))
-                .animate({
-                'top': cart.offset().top + 10,
-                    'left': cart.offset().left + 10,
-                    'width': 75,
-                    'height': 75
-            }, 1000, 'easeInOutExpo');
-            
-            setTimeout(function () {
-                cart.effect("shake", {
-                    times: 2
-                }, 200);
-            }, 1500);
+for (let item of items) {
+  const addToCartButton = item.querySelector(".add-to-cart");
+  addToCartButton.addEventListener("click", () => {
+    const name = item.querySelector("h2").textContent;
+    const priceString = item.querySelector("p").textContent;
+    const price = parseInt(priceString.replace(/[^0-9]/g, ""));
+    shoppingCart.push({ name, price });
+    updateShoppingCart();
+  });
+}
 
-            imgclone.animate({
-                'width': 0,
-                    'height': 0
-            }, function () {
-                $(this).detach()
-            });
-        }
-    });
+function updateShoppingCart() {
+  const shoppingCartElement = document.querySelector(".shopping-cart");
+  shoppingCartElement.textContent = `Shopping Cart (${shoppingCart.length})`;
+}
