f6d5ea3b00b7fb6c890cb8f6c7a49134ce1167b3
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-02-10 09:13:14 -0600

    Update javascript.js

--- CaseProject/Shop/javascript.js
+++ CaseProject/Shop/javascript.js
@@ -1,5 +1,6 @@
 const items = document.querySelectorAll(".item");
 const shoppingCart = [];
+const shoppingCartElement = document.querySelector(".shopping-cart");
 
 for (let item of items) {
   const addToCartButton = item.querySelector(".add-to-cart");
@@ -12,7 +13,32 @@ for (let item of items) {
   });
 }
 
+shoppingCartElement.addEventListener("click", () => {
+  const cart = document.createElement("div");
+  cart.classList.add("cart");
+  document.body.appendChild(cart);
+  
+  const title = document.createElement("h2");
+  title.textContent = "Shopping Cart";
+  cart.appendChild(title);
+  
+  const closeButton = document.createElement("button");
+  closeButton.textContent = "Close";
+  closeButton.addEventListener("click", () => {
+    cart.remove();
+  });
+  cart.appendChild(closeButton);
+  
+  const list = document.createElement("ul");
+  cart.appendChild(list);
+  
+  for (const { name, price } of shoppingCart) {
+    const item = document.createElement("li");
+    item.textContent = `${name}: $${price}`;
+    list.appendChild(item);
+  }
+});
+
 function updateShoppingCart() {
-  const shoppingCartElement = document.querySelector(".shopping-cart");
   shoppingCartElement.textContent = `Shopping Cart (${shoppingCart.length})`;
 }
