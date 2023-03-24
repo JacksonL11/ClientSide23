41ef395ad18c5da295baa7a07f642af7bf934a9b
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-03-23 23:11:13 -0500

    Update javascript.js

--- CaseProject/javascript.js
+++ CaseProject/javascript.js
@@ -1,99 +1,48 @@
 
 
-// Get form element
-const form = document.querySelector('form');
-
-// Add submit event listener to form
+const form = document.getElementById('verification-form');
 form.addEventListener('submit', function(event) {
-  // Prevent form submission
   event.preventDefault();
-
-  // Get form input elements
+  const ageCheckbox = document.getElementById('age-checkbox');
   const firstNameInput = document.getElementById('firstName');
   const lastNameInput = document.getElementById('lastName');
   const dobInput = document.getElementById('dob');
   const licenseNumInput = document.getElementById('licenseNum');
   const stateInput = document.getElementById('state');
+  const hasInsuranceSelect = document.getElementById('hasInsurance');
 
-  // Get form input values
-  const firstNameValue = firstNameInput.value.trim();
-  const lastNameValue = lastNameInput.value.trim();
-  const dobValue = dobInput.value.trim();
-  const licenseNumValue = licenseNumInput.value.trim();
-  const stateValue = stateInput.value.trim();
-
-  // Validate input values
-  if (!isValidName(firstNameValue)) {
-    firstNameInput.classList.add('invalid');
-  } else {
-    firstNameInput.classList.remove('invalid');
+  if (!ageCheckbox.checked) {
+    alert('You must be older than 18 to continue.');
+    return;
   }
 
-  if (!isValidName(lastNameValue)) {
-    lastNameInput.classList.add('invalid');
-  } else {
-    lastNameInput.classList.remove('invalid');
+  if (!firstNameInput.checkValidity()) {
+    alert('Please enter a valid first name.');
+    return;
   }
 
-  if (!isValidDate(dobValue)) {
-    dobInput.classList.add('invalid');
-  } else {
-    dobInput.classList.remove('invalid');
+  if (!lastNameInput.checkValidity()) {
+    alert('Please enter a valid last name.');
+    return;
   }
 
-  if (!isValidLicenseNum(licenseNumValue)) {
-    licenseNumInput.classList.add('invalid');
-  } else {
-    licenseNumInput.classList.remove('invalid');
+  if (!dobInput.checkValidity()) {
+    alert('Please enter a valid date of birth (MM/DD/YYYY).');
+    return;
   }
 
-  if (!isValidState(stateValue)) {
-    stateInput.classList.add('invalid');
-  } else {
-    stateInput.classList.remove('invalid');
+  if (!licenseNumInput.checkValidity()) {
+    alert('Please enter a valid driver\'s license number (8 characters).');
+    return;
   }
 
-  // If all input values are valid, submit form
-  if (isValidName(firstNameValue) &&
-      isValidName(lastNameValue) &&
-      isValidDate(dobValue) &&
-      isValidLicenseNum(licenseNumValue) &&
-      isValidState(stateValue)) {
-    form.submit();
+  if (!stateInput.checkValidity()) {
+    alert('Please enter a valid state abbreviation (2 characters).');
+    return;
   }
+
+  // Form is valid, so you can submit the form and access the rest of the website here
 });
-
-// Validation functions
-function isValidName(name) {
-  const regex = /^[a-zA-Z]+$/;
-  return regex.test(name);
-}
-
-function isValidDate(date) {
-  const regex = /^(0[1-9]|1[0-2])\/([0-2][1-9]|3[0-1])\/\d{4}$/;
-  return regex.test(date);
-}
-
-function isValidLicenseNum(licenseNum) {
-  const regex = /^[A-Za-z0-9]{8}$/;
-  return regex.test(licenseNum);
-}
-
-function isValidState(state) {
-  const regex = /^[A-Za-z]{2}$/;
-  return regex.test(state);
-}
-
-}
-
-
-const tabBtns = document.querySelectorAll('.tab-btn');
-const tabContents = document.querySelectorAll('.tab-content');
-tabBtns.forEach((btn) => {
-  btn.addEventListener('click', () => {
-    const target = document.querySelector(btn.dataset.target);
-    function ValidateEmail(input) {
-      var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
       try {
       (input.value.match(validRegex)) 
