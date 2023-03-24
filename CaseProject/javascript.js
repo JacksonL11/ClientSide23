924a43fc8bd5dfd67ced901aa47b2b4dfb8dfe52
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-03-23 22:49:50 -0500

    Update javascript.js

--- CaseProject/javascript.js
+++ CaseProject/javascript.js
@@ -1,3 +1,21 @@
+window.onload = function() {
+  var popup = document.getElementById("popup-form");
+  var form = document.getElementById("verification-form");
+  var close = document.getElementById("close-popup");
+  
+  popup.style.display = "flex";
+  
+  form.addEventListener("submit", function(event) {
+    event.preventDefault(); // prevent form from submitting
+    popup.style.display = "none";
+    // process form data here
+  });
+  
+  close.onclick = function() {
+    popup.style.display = "none";
+  }
+}
+
 const tabBtns = document.querySelectorAll('.tab-btn');
 const tabContents = document.querySelectorAll('.tab-content');
 tabBtns.forEach((btn) => {
