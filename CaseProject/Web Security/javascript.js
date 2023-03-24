2ae30c9665c7291925b119a0b7f803d67cc83c0e
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-03-23 21:07:23 -0500

    Update javascript.js

--- CaseProject/Web Security/javascript.js
+++ CaseProject/Web Security/javascript.js
@@ -1,1 +1,11 @@
+if (navigator.appName === "Google Chrome") {
+   alert("You are using Google Chrome");
+} else if (navigator.appName === "Microsoft Internet Explorer") {
+   alert("You are using Microsoft Internet Explorer");
+} else if (navigator.appName === "Safari") {
+   alert("You are using Safari");
+}
+
+   // code for other browsers
+}
 
