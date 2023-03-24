214a227361a02f7d70908a387bf2f6fd8591a22f
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-03-23 21:07:12 -0500

    Update javascript.js

--- CaseProject/javascript.js
+++ CaseProject/javascript.js
@@ -42,14 +42,3 @@ tabBtns.forEach((btn) => {
     }
   });
 });
-
-if (navigator.appName === "Google Chrome") {
-   alert("You are using Google Chrome");
-} else if (navigator.appName === "Microsoft Internet Explorer") {
-   alert("You are using Microsoft Internet Explorer");
-} else if (navigator.appName === "Safari") {
-   alert("You are using Safari");
-}
-
-   // code for other browsers
-}
