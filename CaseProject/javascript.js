ead77ae3f3305907e3f8c65d0bf072ca9cf1ac84
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-03-30 09:00:27 -0500

    Update javascript.js

--- CaseProject/javascript.js
+++ CaseProject/javascript.js
@@ -10,3 +10,26 @@ verificationForm.addEventListener("submit", function(event) {
   popupForm.style.display = "none"; // hide the popup form
 });
 
+// Initialize an empty array to store user selections
+let userSelections = [];
+
+
+const checkboxes = document.querySelectorAll('input[type=checkbox]');
+checkboxes.forEach(checkbox => {
+  checkbox.addEventListener('change', function() {
+   
+    if (this.checked) {
+      userSelections.push(this.value);
+    } else { 
+      const index = userSelections.indexOf(this.value);
+      if (index > -1) {
+        userSelections.splice(index, 1);
+      }
+    }
+  });
+});
+
+const options = ['18-20', '21-30', '31-40', '41-50', '51-60', '60+'];
+
+
+const selectionsString = userSelections.join(', '); // e.g. "18-20, 31-40, 51-60"
