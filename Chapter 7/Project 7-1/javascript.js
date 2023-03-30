eef07328073285c38e6dbd9fea437f4154c9db17
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-03-30 08:34:29 -0500

    Update javascript.js

--- Chapter 7/Project 7-1/javascript.js
+++ Chapter 7/Project 7-1/javascript.js
@@ -4,26 +4,19 @@
       Project 07-01
 
       Project to validate a form used for setting up a new account
-      Author: Tyree
-      Date:   
+      Author: Jackson Lawrence
+      Date:   03/30/23
 
       Filename: project07-01.js
 */
-
-
 let signupForm = document.getElementById("signup");
-
 signupForm.addEventListener("submit", function(e) {
 e.preventDefault(); // prevent the browser from responding to the submit event
 let pwd = document.getElementById("pwd").value;
 let feedback = document.getElementById("feedback");
-
-// Create the regular expression variables
 let regex1 = /[A-Z]/;
 let regex2 = /\d/;
 let regex3 = /[!$#%]/;
-
-// Check the password against the regular expressions and provide feedback
 if (pwd.length < 8) {
 feedback.textContent = "Your password must be at least 8 characters.";
 } else if (!regex1.test(pwd)) {
@@ -36,4 +29,6 @@ feedback.textContent = "Your password must include one of the following: !$#%.";
 signupForm.submit(); // Submit the form for processing
 }
 });
+
+
 
