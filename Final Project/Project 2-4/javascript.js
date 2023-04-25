0fdea941073b5c4993c7782227a330a7d98dd63e
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-04-25 18:43:08 -0500

    Create javascript.js

---
+++ Final Project/Project 2-4/javascript.js
@@ -0,0 +1,19 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-04
+
+      Application to calculate the cost of a restaurant order plus tax
+      Author: Jackson Lawrence
+      Date:   04/25/23
+
+      Filename: project02-04.js
+ */
+ 
+
+
+
+
+// Function to display a numeric value as a text string in the format $##.## 
+ function formatCurrency(value) {
+    return "$" + value.toFixed(2);
+ }
