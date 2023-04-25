5e41e6b56c16cf845070b78ec67b07e4d22c763a
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-04-25 11:17:22 -0500

    Update javascript.js

--- Final Project/Project 9-4/javascript.js
+++ Final Project/Project 9-4/javascript.js
@@ -7,53 +7,26 @@ let clockTimer = document.getElementById("timer");
 // Custom event that runs when the puzzle is solved
 window.addEventListener("puzzleSolved", updateRecord);
 
-// Event listener that is run when the page loads
 window.addEventListener("load", function() {
-  // Check if localStorage is available
-  if (typeof(Storage) !== "undefined") {
-    // Get the user's best time from localStorage
-    let bestTime = localStorage.getItem("bestTime");
-    // If the user has a best time, update the bestText object
-    if (bestTime) {
-      bestText.textContent = `${bestTime} seconds`;
-    }
+  if (localStorage.getItem("puzzle8Best")) {
+    bestText.textContent = getBestTime() + " seconds";
   }
 });
 
-/// Function to update the user's best time in localStorage
-function updateRecord(event) {
-  // Get the user's current time
-  let currentTime = clockTimer.textContent;
-  // Get the user's best time from localStorage
-  let bestTime = localStorage.getItem("bestTime");
-  // If the user doesn't have a best time, set it to the current time
-  if (!bestTime) {
-    localStorage.setItem("bestTime", currentTime);
-    bestText.textContent = `${currentTime} seconds`;
-  }
-  // If the user has a best time, compare it to the current time and update it if necessary
-  else {
-    if (currentTime < bestTime) {
-      localStorage.setItem("bestTime", currentTime);
-      bestText.textContent = `${currentTime} seconds`;
-    }
-  }
-}
-
-
-// Function to get the user's best time from localStorage
 function getBestTime() {
-  // If the localStorage key exists, return the integer value
-  let bestTime = localStorage.getItem('bestTime');
-  if (bestTime !== null) {
-    return parseInt(bestTime);
-  }
-  // If the localStorage key doesn't exist, return 9999
-  else {
+  if (localStorage.getItem("puzzle8Best")) {
+    return parseInt(localStorage.getItem("puzzle8Best"));
+  } else {
     return 9999;
   }
 }
 
-// Event listener that is run when the page unloads
-window.addEventListener("unload", function() {
-  // Remove the "bestTime" key from localStorage
+function updateRecord() {
+  let solutionTime = parseInt(document.getElementById("timer").value);
+  let bestTime = getBestTime();
+  if (solutionTime < bestTime) {
+    bestTime = solutionTime;
+    bestText.textContent = bestTime + " seconds";
+    localStorage.setItem("puzzle8Best", bestTime);
+  }
+}
