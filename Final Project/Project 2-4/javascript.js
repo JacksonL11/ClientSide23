1b7e4f9ac7be98e2cd00d861f9dbb7ee1428a0bb
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-04-25 22:01:17 -0500

    Update javascript.js

--- Final Project/Project 2-4/javascript.js
+++ Final Project/Project 2-4/javascript.js
@@ -14,23 +14,25 @@
 
 
 // Function to display a numeric value as a text string in the format $##.## 
- function formatCurrency(value) {
-    return "$" + value.toFixed(2);
- }
+ function calculateTotal() {
+  var chickenQty = parseInt(document.getElementById('chicken').value);
+  var halibutQty = parseInt(document.getElementById('halibut').value);
+  var burgerQty = parseInt(document.getElementById('burger').value);
+  var salmonQty = parseInt(document.getElementById('salmon').value);
+  var saladQty = parseInt(document.getElementById('salad').value);
 
-   function calculateTotal() {
-      var chickenQty = parseInt(document.getElementById('chicken').value);
-      var halibutQty = parseInt(document.getElementById('halibut').value);
-      var burgerQty = parseInt(document.getElementById('burger').value);
-      var salmonQty = parseInt(document.getElementById('salmon').value);
-      var saladQty = parseInt(document.getElementById('salad').value);
+  // Make sure quantities are not negative
+  chickenQty = chickenQty < 0 ? 0 : chickenQty;
+  halibutQty = halibutQty < 0 ? 0 : halibutQty;
+  burgerQty = burgerQty < 0 ? 0 : burgerQty;
+  salmonQty = salmonQty < 0 ? 0 : salmonQty;
+  saladQty = saladQty < 0 ? 0 : saladQty;
 
-      var foodTotal = chickenQty * 10.95 + halibutQty * 13.95 + burgerQty * 9.95 + salmonQty * 18.95 + saladQty * 7.95;
-      var foodTax = foodTotal * 0.1;
-      var totalBill = foodTotal + foodTax;
+  var foodTotal = chickenQty * 10.95 + halibutQty * 13.95 + burgerQty * 9.95 + salmonQty * 18.95 + saladQty * 7.95;
+  var foodTax = foodTotal * 0.1;
+  var totalBill = foodTotal + foodTax;
 
-      document.getElementById('foodTotal').innerHTML = '$' + foodTotal.toFixed(2);
-      document.getElementById('foodTax').innerHTML = '$' + foodTax.toFixed(2);
-      document.getElementById('totalBill').innerHTML = '$' + totalBill.toFixed(2);
-   }
-   
+  document.getElementById('foodTotal').innerHTML = '$' + foodTotal.toFixed(2);
+  document.getElementById('foodTax').innerHTML = '$' + foodTax.toFixed(2);
+  document.getElementById('totalBill').innerHTML = '$' + totalBill.toFixed(2);
+}
