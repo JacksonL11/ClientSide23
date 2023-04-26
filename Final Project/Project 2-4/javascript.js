92218a64a4064b72d1cd8f34a01f771f02b87e6a
JacksonL11 <122898655+JacksonL11@users.noreply.github.com>
2023-04-25 21:42:23 -0500

    Update javascript.js

--- Final Project/Project 2-4/javascript.js
+++ Final Project/Project 2-4/javascript.js
@@ -17,3 +17,18 @@
  function formatCurrency(value) {
     return "$" + value.toFixed(2);
  }
+  function calculateTotal() {
+      var chickenQty = parseInt(document.getElementById('chicken').value);
+      var halibutQty = parseInt(document.getElementById('halibut').value);
+      var burgerQty = parseInt(document.getElementById('burger').value);
+      var salmonQty = parseInt(document.getElementById('salmon').value);
+      var saladQty = parseInt(document.getElementById('salad').value);
+
+      var foodTotal = chickenQty * 10.95 + halibutQty * 13.95 + burgerQty * 9.95 + salmonQty * 18.95 + saladQty * 7.95;
+      var foodTax = foodTotal * 0.1;
+      var totalBill = foodTotal + foodTax;
+
+      document.getElementById('foodTotal').innerHTML = '$' + foodTotal.toFixed(2);
+      document.getElementById('foodTax').innerHTML = '$' + foodTax.toFixed(2);
+      document.getElementById('totalBill').innerHTML = '$' + totalBill.toFixed(2);
+   }
